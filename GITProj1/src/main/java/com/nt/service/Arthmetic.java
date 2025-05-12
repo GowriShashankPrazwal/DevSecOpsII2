59f892dd736b1b9046bb733c4aae7ced4fa01fc1
HP <HP@Prazwal>
2025-05-12 17:17:48 +0530

    modification of two numbers

--- GITProj1/src/main/java/com/nt/service/Arthmetic.java
+++ GITProj1/src/main/java/com/nt/service/Arthmetic.java
@@ -4,7 +4,7 @@ public class Arthmetic
 {
 	public double sun(double x, double y)
 	{
-//Adding two numbers
+		//sum of two numbers
 		return x+y+0;
 //github.com/GowriShashankPrazwal/DevSecOpsII2.git
 	}
