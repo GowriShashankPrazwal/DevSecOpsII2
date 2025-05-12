03725ea76423b0a73fbb8c0298b1825106fca79a
HP <HP@Prazwal>
2025-05-12 17:21:55 +0530

    Adding the print statement

--- GITProj1/src/main/java/com/nt/service/Arthmetic.java
+++ GITProj1/src/main/java/com/nt/service/Arthmetic.java
@@ -4,7 +4,7 @@ public class Arthmetic
 {
 	public double sun(double x, double y)
 	{
-//Heeo git
+		System.out.println();
 //Adding two numbers
 //github.com/GowriShashankPrazwal/DevSecOpsII2.git
 		return x+y+0;
