32cd706bbf413d0fd7c7b27c30d3c3e53fb53ecd
HP <HP@Prazwal.mshome.net>
2025-05-13 14:07:22 +0530

    Dev1 --completed UPIPayment features

---
+++ GITProj1/src/main/java/com/nt/upi/UpiPaymentOperation.java
@@ -0,0 +1,9 @@
+package com.nt.upi;
+
+public class UpiPaymentOperation 
+{
+	public String doPayment(long PhNo,double amount)
+	{
+		return amount+" Amount is paid to Phone Number :: "+PhNo;
+	}
+}
