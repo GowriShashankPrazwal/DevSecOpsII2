68b10031713f5c8e2641b80809c39b18f3f7194c
HP <HP@Prazwal>
2025-05-13 16:23:11 +0530

    Tl Changed the code

--- GITProj1/src/main/java/com/nt/nb/NetBankingService.java
+++ GITProj1/src/main/java/com/nt/nb/NetBankingService.java
@@ -2,8 +2,9 @@ package com.nt.nb;
 
 public class NetBankingService 
 {
+	//Bt default performs RTGS Payment
 	public String transferMoney(long srcAccpount,long destAccount,long amount)
 	{
-		return amount+" is transfering from "+srcAccpount+" to "+destAccount;
+		return amount+" is transfering from "+srcAccpount+" to   "+destAccount;
 	}
 }
