25232a145d1046952aacc580750f95578c6753c3
HP <HP@Prazwal.mshome.net>
2025-05-13 16:59:54 +0530

    UPIid added successfully

--- GITProj1/src/main/java/com/nt/upi/UpiPaymentOperation.java
+++ GITProj1/src/main/java/com/nt/upi/UpiPaymentOperation.java
@@ -3,7 +3,7 @@ package com.nt.upi;
 public class UpiPaymentOperation 
 {
 	//Performs the upi payment
-	public String doPayment(long PhNo,double amount)
+	public String doPayment(long PhNo,double amount,String UPIid)
 	{
 		return amount+" Amount is paid to @phone number :: "+PhNo;
 	}
