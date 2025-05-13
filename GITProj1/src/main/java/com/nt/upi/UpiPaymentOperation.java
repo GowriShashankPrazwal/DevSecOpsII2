0b9fa585b0ffa706f4d158ebb5332cc7a2ddf404
HP <HP@Prazwal>
2025-05-13 14:23:58 +0530

    TL reviewd branch ciode

--- GITProj1/src/main/java/com/nt/upi/UpiPaymentOperation.java
+++ GITProj1/src/main/java/com/nt/upi/UpiPaymentOperation.java
@@ -2,8 +2,9 @@ package com.nt.upi;
 
 public class UpiPaymentOperation 
 {
+	//Performs the upi payment
 	public String doPayment(long PhNo,double amount)
 	{
-		return amount+" Amount is paid to Phone Number :: "+PhNo;
+		return amount+" Amount is paid to @phone number :: "+PhNo;
 	}
 }
