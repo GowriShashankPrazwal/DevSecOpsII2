ca4e905240be1c408b04c84e9739a47280b60a30
HP <HP@Prazwal>
2025-05-13 16:40:46 +0530

    Tag Added payment

--- GITProj1/src/main/java/com/nt/nb/NetBankingService.java
+++ GITProj1/src/main/java/com/nt/nb/NetBankingService.java
@@ -5,6 +5,7 @@ public class NetBankingService
 	//Bt default performs RTGS Payment
 	public String transferMoney(long srcAccpount,long destAccount,long amount)
 	{
+		//Perform the UPI Payment
 		return amount+" is transfering from "+srcAccpount+" to   "+destAccount;
 	}
 }
